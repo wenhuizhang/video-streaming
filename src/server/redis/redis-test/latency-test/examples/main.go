66259965a315d7ff22e32efba6d125ccfbbd5a5d
wenhuizhang <wenhui@gwmail.gwu.edu>
2019-06-11 22:37:58 -0400

    add latency test

---
+++ src/server/redis/redis-test/latency-test/examples/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/tylertreat/bench"
+	"github.com/tylertreat/bench/requester"
+)
+
+func main() {
+	r := &requester.RedisPubSubRequesterFactory{
+		URL:         "10.10.0.14:6379",
+		PayloadSize: 117000,
+		Channel:     "benchmark",
+	}
+
+	benchmark := bench.NewBenchmark(r, 10000, 8, 30*time.Second, 0)
+	summary, err := benchmark.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(summary)
+	summary.GenerateLatencyDistribution(nil, "redis.txt")
+}
