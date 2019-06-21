dd06dce57f368c76ac6d24b3d499c5ac56a96cef
wenhuizhang <wenhui@gwmail.gwu.edu>
2019-06-21 14:33:03 -0400

    add test suit

--- src/server/redis/redis-test/latency-test/examples/main.go
+++ src/server/redis/redis-test/latency-test/examples/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
+	"os"
+	"strconv"
 
 	"github.com/tylertreat/bench"
 	"github.com/tylertreat/bench/requester"
@@ -11,11 +13,17 @@ import (
 func main() {
 	r := &requester.RedisPubSubRequesterFactory{
 		URL:         "10.10.0.14:6379",
-		PayloadSize: 117000,
+		PayloadSize: 1843200,
 		Channel:     "benchmark",
 	}
 
-	benchmark := bench.NewBenchmark(r, 10000, 128, 30*time.Second, 0)
+	var sub_num int64
+	var subs_num uint64
+	sub_num, err := strconv.ParseInt(os.Args[1], 10, 64)
+	subs_num = uint64(sub_num)
+	fmt.Println(subs_num)
+
+	benchmark := bench.NewBenchmark(r, 10000, subs_num, 30*time.Second, 0)
 	summary, err := benchmark.Run()
 	if err != nil {
 		panic(err)
