7ec250f9773ee84ec22ce2f01837ad8cbc00e9f7
Wenhui Zhang <wenhui@gwmail.gwu.edu>
2019-07-24 15:35:00 -0400

    Update run.sh

--- src/server/redis/redis-test/latency-test/examples/run.sh
+++ src/server/redis/redis-test/latency-test/examples/run.sh
@@ -6,12 +6,14 @@ set -m
 rm redis.txt
 rm uncorrected_redis.txt
 
-
-for i in {1..522..1}
+#for j in {7843200,8843200,9843200,11500000,12500000,13500000,14500000,15500000,16500000,17500000,18500000}
+#for j in {117000,1843200,2843200,3843200,4147200,4843200,5843200,6843200,7843200,8843200,9843200,11500000}
+for j in {117000..117000..1}
 do
-	go run main.go $i
-	mv redis.txt redis_$i.txt
-	rm uncorrected_redis.txt
-done
-
-		 
+	for i in {1..512..1}
+	do
+		/usr/local/go/bin/go run main.go $i $j
+		mv redis.txt redis_size_$i.txt
+		rm uncorrected_redis.txt
+	done
+done		 
